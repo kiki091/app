6a86ee193326c5e2ac06879af2061c1dc100b14d
Kiki Kurniawan <kikikurniawan091@gmail.com>
2016-11-30 00:28:53 +0700

    update field
    update field and create new

--- application/modules/ticket/models/ticket_m.php
+++ application/modules/ticket/models/ticket_m.php
@@ -3,22 +3,24 @@
 class Ticket_m extends CI_Model 
 {
 	public function create($input = '')
-    {
-        $create = $this->db->insert('ticket', array(
-			'code' => $input['code'],
-			'problem_status' => $input['problem_status'],
-            'assignee' => $input['assignee'],
-            'title' => $input['title'],
-            'category' => $input['category'],
-            'recipient' => $input['recipient'],
-            'branch' => $input['branch'],
-            'office_name' => $input['office'],
-            'description' => $input['description'],
-            'time_create' => date('Y-m-d H:i:s'),
-            'ip_address' => $this->input->ip_address(),
-            'is_active' => '0',
-        ));
+    	{
+        	$create = $this->db->insert('ticket', array(
+			'code'              => $input['code'],
+			'problem_status'    => $input['problem_status'],
+			'assignee'          => $input['assignee'],
+			'title'             => $input['title'],
+			'category'          => $input['category'],
+			'recipient'         => $input['recipient'],
+			'team_viewer_id'    => $input['team_viewer_id'],
+			'team_viewer_pass'  => $input['team_viewer_pass'],
+			'branch'            => $input['branch'],
+			'office_name'       => $input['office'],
+			'description'       => $input['description'],
+			'time_create'       => date('Y-m-d H:i:s'),
+			'ip_address'        => $this->input->ip_address(),
+			'is_active'         => '0',
+        	));
 		
 		return $create;
-    }
+    	}
 }
